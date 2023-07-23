d0a2202e371fbce81b616a84aaab754d8ff62ca9
mostaphahmed <122462024+mostaphahmed@users.noreply.github.com>
2023-07-23 17:22:42 +0200

    Add files via upload

---
+++ sample_creator_strata_unit_based_auto.py
@@ -0,0 +1,162 @@
+
+## Import libraries in python
+import gc
+import argparse
+import os
+import json
+import logging
+import sys
+import h5py
+import time
+import matplotlib
+import numpy as np
+import pandas as pd
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+import math
+import random
+import importlib
+from scipy.stats import randint, expon, uniform
+import sklearn as sk
+from sklearn import svm
+from sklearn.utils import shuffle
+from sklearn import metrics
+from sklearn import preprocessing
+from sklearn import pipeline
+# from sklearn.metrics import mean_squared_error
+from math import sqrt
+import scipy.stats as stats
+
+
+# from utils.data_preparation_unit_6_Train_3_Test_Updated_32_Features import df_all_creator, df_train_creator, df_test_creator, Input_Gen
+
+#from utils.data_preparation_strata_unit_based import df_all_creator, df_train_creator, df_test_creator, Input_Gen
+
+#from utils.data_preparation_unit_based_variable_sample_per_unit_108139 import df_all_creator, df_train_creator, df_test_creator, Input_Gen
+
+#from utils.data_preparation_strata_unit_based_PCA import df_all_creator, df_train_creator, df_test_creator, Input_Gen
+
+from utils.data_preparation_strata_unit_based import df_all_creator, df_train_creator,df_test_creator, Input_Gen
+
+seed = 0
+random.seed(0)
+np.random.seed(seed)
+
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+data_filedir = os.path.join(current_dir, 'N-CMAPSS')
+data_filepath = os.path.join(current_dir, 'N-CMAPSS', 'N-CMAPSS_DS02-006.h5')
+
+
+
+
+def main():
+    # current_dir = os.path.dirname(os.path.abspath(__file__))
+    parser = argparse.ArgumentParser(description='sample creator')
+    parser.add_argument('-w', type=int, default=10, help='window length', required=True)
+    parser.add_argument('-s', type=int, default=10, help='stride of window')
+    parser.add_argument('--sampling', type=int, default=1, help='sub sampling of the given data. If it is 10, then this indicates that we assumes 0.1Hz of data collection')
+    parser.add_argument('--test', type=int, default='non', help='select train or test, if it is zero, then extract samples from the engines used for training')
+
+    args = parser.parse_args()
+
+    sequence_length = args.w
+    stride = args.s
+    sampling = args.sampling
+    selector = args.test
+
+
+
+    # Load data
+    '''
+    W: operative conditions (Scenario descriptors)
+    X_s: measured signals
+    X_v: virtual sensors
+    T(theta): engine health parameters
+    Y: RUL [in cycles]
+    A: auxiliary data
+    '''
+
+    df_all = df_all_creator(data_filepath, sampling)
+
+    '''
+    Split dataframe into Train and Test
+    Training units: 2, 5, 10, 16, 18, 20
+    Test units: 11, 14, 15
+
+    '''
+
+    units_index_train = [2.0, 5.0, 10.0,11.0,14.0,15.0,16.0,18.0,20.0]
+    units_index_vlad = [2.0, 5.0, 10.0,11.0,14.0,15.0,16.0,18.0,20.0]
+    units_index_test = [2.0, 5.0, 10.0,11.0,14.0,15.0,16.0,18.0,20.0]
+
+    print("units_index_train", units_index_train)
+    print("units_index_test", units_index_test)
+
+    # if any(int(idx) == unit_index for idx in units_index_train):
+    #     df_train = df_train_creator(df_all, units_index_train)
+    #     print(df_train)
+    #     print(df_train.columns)
+    #     print("num of inputs: ", len(df_train.columns) )
+    #     df_test = pd.DataFrame()
+    #
+    # else :
+    #     df_test = df_test_creator(df_all, units_index_test)
+    #     print(df_test)
+    #     print(df_test.columns)
+    #     print("num of inputs: ", len(df_test.columns))
+    #     df_train = pd.DataFrame()
+
+    df_train, df_vlad = df_train_creator(df_all, units_index_train)
+    #df_train = shuffle(df_train)
+    print(df_train)
+    print(df_train.columns)
+    print("num of inputs: ", len(df_train.columns))
+   
+    print(df_vlad)
+    print(df_vlad.columns)
+    print("num of inputs: ", len(df_vlad.columns))
+   
+    df_test = df_test_creator(df_all, units_index_test)
+    print(df_test)
+    print(df_test.columns)
+    print("num of inputs: ", len(df_test.columns))
+
+
+    del df_all
+    gc.collect()
+    df_all = pd.DataFrame()
+    sample_dir_path = os.path.join(data_filedir, 'Samples_whole')
+    sample_folder = os.path.isdir(sample_dir_path)
+    if not sample_folder:
+        os.makedirs(sample_dir_path)
+        print("created folder : ", sample_dir_path)
+
+    cols_normalize = df_train.columns.difference(['RUL', 'unit'])
+    sequence_cols = df_train.columns.difference(['RUL', 'unit'])
+
+
+    if selector == 0:
+        for unit_index in units_index_train:
+            print ("you are in selector for train")
+            data_class = Input_Gen (df_train,df_vlad, df_test, cols_normalize, sequence_length, sequence_cols, sample_dir_path,
+                                    unit_index, sampling, stride =stride)
+            data_class.seq_gen()
+        for unit_index in units_index_train:
+            print ("you are in selector for vlad")
+            data_class = Input_Gen (df_train,df_vlad, df_test, cols_normalize, sequence_length, sequence_cols, sample_dir_path,
+                                    unit_index, sampling, stride =stride)
+            data_class.seq_gen_vlad()    
+        for unit_index in units_index_test:
+            print ("you are in selector for test")
+            data_class = Input_Gen (df_train,df_vlad,df_test, cols_normalize, sequence_length, sequence_cols, sample_dir_path,
+                                    unit_index, sampling, stride =stride)
+            data_class.seq_gen_test()
+
+    else:
+        print("ERROR, PLEASE ENTER 0")
+
+
+if __name__ == '__main__':
+    main()
